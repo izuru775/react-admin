daf394e724c1280de53b385893c8465b9b7ca20c
izuru775 <izuru775@gmail.com>
2022-01-20 12:34:32 +1100

    React admin completed

---
+++ src/components/PostList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  DateField,
+  EditButton,
+  DeleteButton,
+} from "react-admin";
+
+const PostList = (props) => {
+  return <List {...props}>
+      <Datagrid>
+          <TextField source="id"></TextField>
+          <TextField source="title"></TextField>
+          <DateField source="publishedAt"></DateField>
+          <EditButton basePath="/posts"></EditButton>
+          <DeleteButton basePath="/posts"></DeleteButton>
+      </Datagrid>
+  </List>;
+};
+
+export default PostList;
